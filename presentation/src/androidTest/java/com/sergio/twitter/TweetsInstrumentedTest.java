78078606014326b57b035331eca51d40a105f6f5
Sergio Carabantes <sergio.carabantes@gmail.com>
2018-07-11 23:29:22 +0100

    Added a new ui test.

--- presentation/src/androidTest/java/com/sergio/twitter/TweetsInstrumentedTest.java
+++ presentation/src/androidTest/java/com/sergio/twitter/TweetsInstrumentedTest.java
@@ -16,27 +16,66 @@
 
 package com.sergio.twitter;
 
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import com.sergio.twitter.tweets.ui.TweetsActivity;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class TweetsInstrumentedTest {
 
-    @Rule public ActivityTestRule<TweetsActivity> activityTestRule = new ActivityTestRule(
-            TweetsActivity.class);
+    @Rule public ActivityTestRule<TweetsActivity> activityTestRule = new ActivityTestRule(TweetsActivity.class);
 
     @Test
-    public void should_show_some_data(){
+    public void should_show_some_data_and_then_click(){
+        if (getRecyclerViewItemCount() > 0){
+            onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        }
+    }
+
+    @Test
+    public void should_show_data_after_searching_a_word_and_then_click(){
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.search), withContentDescription("Search"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction searchAutoComplete = onView(
+                allOf(withId(R.id.search_src_text),
+                        childAtPosition(
+                                allOf(withId(R.id.search_plate),
+                                        childAtPosition(
+                                                withId(R.id.search_edit_frame),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        searchAutoComplete.perform(replaceText("hi"), closeSoftKeyboard());
+
         if (getRecyclerViewItemCount() > 0){
             onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         }
@@ -45,5 +84,24 @@ public class TweetsInstrumentedTest {
     private int getRecyclerViewItemCount(){
         RecyclerView recyclerView = activityTestRule.getActivity().findViewById(R.id.recyclerView);
         return recyclerView.getAdapter().getItemCount();
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
